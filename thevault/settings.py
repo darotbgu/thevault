28aae8f8a50dec394ebfbb18bedca8e5b0d7579a
rotem <darot@post.bgu.ac.il>
2020-03-30 23:14:17 +0300

    added rest framework and token auth

--- thevault/settings.py
+++ thevault/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'thevault.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'vault'),
     }
 }
 
